06693b26dbddef1b9d58470635b0306245a81883
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-03 14:36:59 +0600

    Create E. Area of a Circle.cpp

---
+++ E. Area of a Circle.cpp
@@ -0,0 +1,13 @@
+/*https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E*/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double pi = 3.141592653, R, A;
+    cin >> R;
+    A = pi * (R * R);
+    cout << fixed << setprecision(9) << A << endl;
+    return 0;
+}
