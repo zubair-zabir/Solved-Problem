42f87245c94b67258a4479ec34e766117f60640a
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-03 16:41:29 +0600

    Create F. Reversing.cpp

---
+++ F. Reversing.cpp
@@ -0,0 +1,18 @@
+/*https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/F*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[1000];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for(int i = n - 1; i >= 0; i--)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
