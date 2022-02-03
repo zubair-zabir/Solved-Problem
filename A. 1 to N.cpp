d81ad946d2a9a441212291b591616a37b1c5205a
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-03 14:57:59 +0600

    Create A. 1 to N.cpp

---
+++ A. 1 to N.cpp
@@ -0,0 +1,12 @@
+/*https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/A*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, i;
+    cin >> n;
+    for(i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+}
