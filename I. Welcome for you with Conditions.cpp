b9974c9bd8ea88e415ba8c014235b922b57e6d6b
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-03 14:42:05 +0600

    Create I. Welcome for you with Conditions.cpp

---
+++ I. Welcome for you with Conditions.cpp
@@ -0,0 +1,17 @@
+/*https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I*/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int A, B;
+    cin >> A >> B;
+    if(A >= B)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+        cout << "No" << endl;
+    return 0;
+}
