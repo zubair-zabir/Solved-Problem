e20df684e601a37f88243667b61857a391d2b3c2
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-03 14:39:46 +0600

    Create G. Summation from 1 to N.cpp

---
+++ G. Summation from 1 to N.cpp
@@ -0,0 +1,13 @@
+/*https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G*/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    long long int n, sum;
+    cin >> n;
+    sum = (n*(n + 1)) / 2;
+    cout << sum << endl;
+}
+ 
