ece47b901fb58370adb772bf25000c15bb35a4bf
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-04 15:17:38 +0600

    Create A. Create A New String.cpp

---
+++ A. Create A New String.cpp
@@ -0,0 +1,11 @@
+/*https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s, t;
+    getline(cin, s);
+    getline(cin, t);
+    cout << s.size() << " " << t.size() << endl;
+    cout << s << " " << t;
+}
