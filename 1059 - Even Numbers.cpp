c4819e0ff700836b836fed605d6a4788ce10e258
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-06 16:04:36 +0600

    Create 1059 - Even Numbers.cpp

---
+++ 1059 - Even Numbers.cpp
@@ -0,0 +1,16 @@
+/*https://www.beecrowd.com.br/judge/en/problems/view/1059*/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int i;
+    for(i = 1; i <= 100; i++)
+    {
+        if(i % 2 == 0)
+        {
+            cout << i << endl;
+        }
+    }
+}
