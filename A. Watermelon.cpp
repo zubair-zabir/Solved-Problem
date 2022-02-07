dc44dd0b79e338018ece9fd78b371ece619a7397
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-07 13:31:19 +0600

    Create A. Watermelon.cpp

---
+++ A. Watermelon.cpp
@@ -0,0 +1,12 @@
+/*https://codeforces.com/contest/4/problem/A*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int w;
+   cin >> w;
+   if(w % 2 == 0 && w != 2)
+    cout << "YES" << endl;
+   else 
+    cout << "NO" << endl;
+}
