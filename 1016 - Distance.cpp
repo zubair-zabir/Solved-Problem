d5a492b3d354d36935cfb0071836d867f638be37
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-13 17:40:00 +0600

    Create  1016 - Distance.cpp

---
+++ 1016 - Distance.cpp
@@ -0,0 +1,13 @@
+/*https://www.beecrowd.com.br/judge/en/problems/view/1016*/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int x, y;
+    cin >> x;
+    y= 2 * x;
+    cout << y << " minutos"<<endl;
+    return 0;
+}
