c9ba0891f1daac1a21401de747a3c35675e7c25a
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-13 17:19:17 +0600

    Create  1044 - Multiples.cpp

---
+++ 1044 - Multiples.cpp
@@ -0,0 +1,19 @@
+/*https://www.beecrowd.com.br/judge/en/problems/view/1044*/
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    if((A % B == 0) || (B % A == 0))
+    {
+        cout << "Sao Multiplos" << endl;
+    }
+    else
+    {
+        cout << "Nao sao Multiplos" << endl;
+    }
+    return 0;
+}
