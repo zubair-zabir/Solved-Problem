6f24407990b0331405bc8667827eda8e4de4a6da
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-13 17:47:36 +0600

    Create  1007 - Difference.cpp

---
+++ 1007 - Difference.cpp
@@ -0,0 +1,13 @@
+/*https://www.beecrowd.com.br/judge/en/problems/view/1007*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int A, B, C, D, E;
+    cin >> A >> B >> C >> D;
+    E = (A * B) - (C * D);
+    cout <<"DIFERENCA = " << E <<endl;
+    return 0;
+}
