b9bcb2fa27aa2642534d51ef824df9a39f923140
zubair-zabir <98940151+zubair-zabir@users.noreply.github.com>
2022-02-13 16:40:54 +0600

    Create A. Domino piling.cpp

---
+++ A. Domino piling.cpp
@@ -0,0 +1,10 @@
+/*https://codeforces.com/problemset/problem/50/A*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    int prod = (m * n) / 2;
+    cout << prod << endl;
+}
